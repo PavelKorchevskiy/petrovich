d6a8024097b6d3772ba6fc4edee8a2247d436605
korchevskii.pavel@otr.ru <otrpassword>
2023-02-03 10:56:13 +0300

    Initial commit

---
+++ src/main/java/com/fun/petrovich/PetrovichApplication.java
@@ -0,0 +1,13 @@
+package com.fun.petrovich;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetrovichApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetrovichApplication.class, args);
+	}
+
+}
